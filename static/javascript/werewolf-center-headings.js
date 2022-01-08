fbcf1f56569a61ed885f548fd3c776af15cc2887
MalwareWerewolf <davide.dolce@protonmail.com>
2022-01-09 00:17:21 +0100

    Updated config and custom js

--- static/javascript/werewolf-center-headings.js
+++ static/javascript/werewolf-center-headings.js
@@ -1,12 +1,20 @@
-((d) => {
-    d.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(element =>
+const styleTextCenterProperty = "text-align: center";
+
+(() => {
+    document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(element =>
     {
-        element.style = "text-align: center";
+        element.style = styleTextCenterProperty;
     });
 
-    let postMeta = d.getElementsByClassName("post-meta");
+    let postMeta = document.getElementsByClassName("post-meta");
     if(postMeta.length === 1){
-        postMeta[0].style = "text-align: center";
+        postMeta[0].style = styleTextCenterProperty;
         postMeta[0].insertAdjacentHTML('afterend', '<hr />');
     }
-})(document)
+    else{
+        let title = document.querySelector("h1.title");
+        if(title !== null){
+            title.insertAdjacentHTML('afterend', '<hr />');
+        }       
+    }
+})()
