c4a7f20d8c3f7bd34fa5a75bfe20966a3dacc5eb
MalwareWerewolf <davidedlc97@gmail.com>
2022-01-10 12:16:14 +0100

    Update remove-disqus-ads.js

--- static/javascript/remove-disqus-ads.js
+++ static/javascript/remove-disqus-ads.js
@@ -1,8 +1,8 @@
 let foundDsq = false;
 
 window.addEventListener("load", function () {
-    let elements = document.querySelectorAll('[id^="dsq-"]');
     setInterval(function () {
+        let elements = document.querySelectorAll('[id^="dsq-"]');
         elements.forEach(element => {
             if (elements.length > 1 && !foundDsq) {
                 foundDsq = true;
