fe58c21bea6efb6221647c5e0aa0f71243be96b8
MalwareWerewolf <davide.dolce@protonmail.com>
2022-01-10 20:19:05 +0100

    Updated disqus script

--- static/javascript/remove-disqus-ads.js
+++ static/javascript/remove-disqus-ads.js
@@ -1,13 +1,8 @@
-let foundDsq = false;
-
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    let elements = document.querySelectorAll("#disqus_thread > iframe");
-    elements.forEach((element, index) => {
-      if (elements.length > 1 && !foundDsq && index !== 1) {
-          foundDsq = true;
-          element.style = "display: none";              
-      }
-    })
-  }, 2000);
-});
+window.addEventListener("load", setTimeout(function () {
+  let elements = document.querySelectorAll("#disqus_thread > iframe");
+  elements.forEach((element, index) => {
+    if (elements.length > 1 && index !== 1) {
+      element.style = "display: none";
+    }
+  })
+}, 2000));
