614195dd6a7b32fcee0bf23995f3f5cc754e9c29
MalwareWerewolf <davidedlc97@gmail.com>
2022-01-10 09:50:12 +0100

    Added timeout to Disqus script

--- static/javascript/remove-disqus-ads.js
+++ static/javascript/remove-disqus-ads.js
@@ -1,11 +1,13 @@
 let foundDsq = false;
 
-window.addEventListener("load", function() {
-  let elements = document.querySelectorAll('[id^="dsq-"]');
-  elements.forEach(element => {
-    if(elements.length > 1 && !foundDsq){
-        foundDsq = true;
-        element.style = "display: none";
-    }
-  })
+window.addEventListener("load", function () {
+    let elements = document.querySelectorAll('[id^="dsq-"]');
+    setTimeout(function () {
+        elements.forEach(element => {
+            if (elements.length > 1 && !foundDsq) {
+                foundDsq = true;
+                element.style = "display: none";
+            }
+        })
+    }, 2000);
 });
