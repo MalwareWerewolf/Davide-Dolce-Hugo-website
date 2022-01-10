db0aa69c36508fd5d79132f768317f331ff91d6b
MalwareWerewolf <davide.dolce@protonmail.com>
2022-01-10 20:04:21 +0100

    Updated queryselector in disqus script

--- static/javascript/remove-disqus-ads.js
+++ static/javascript/remove-disqus-ads.js
@@ -1,11 +1,13 @@
 let foundDsq = false;
 
 window.addEventListener("load", function () {
-  let elements = document.querySelectorAll("#disqus_thread > iframe");
-  elements.forEach((element, index) => {
+  setTimeout(function () {
+    let elements = document.querySelectorAll("#disqus_thread > iframe");
+    elements.forEach((element, index) => {
       if (elements.length > 1 && !foundDsq && index !== 1) {
           foundDsq = true;
           element.style = "display: none";              
       }
-  })
+    })
+  }, 2000);
 });
