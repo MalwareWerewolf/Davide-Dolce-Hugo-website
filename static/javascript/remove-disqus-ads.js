685693b9ee1699a006536e9ab0eb09aee9fbefc3
MalwareWerewolf <davide.dolce@protonmail.com>
2022-01-13 19:16:57 +0100

    Removed Disqus and Goat Counter

--- static/javascript/remove-disqus-ads.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", setTimeout(function () {
-  let elements = document.querySelectorAll("#disqus_thread > iframe");
-  elements.forEach((element, index) => {
-    if (elements.length > 1 && index !== 1) {
-      element.style = "display: none";
-    }
-  })
-}, 2000));
